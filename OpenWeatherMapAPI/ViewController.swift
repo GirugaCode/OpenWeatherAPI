2300caa0e253409bce6b5eb6bf182e99fc1d1777
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-08-07 20:07:44 -0700

    [init] Set up views

---
+++ OpenWeatherMapAPI/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  OpenWeatherMapAPI
+//
+//  Created by Ryan Nguyen on 8/7/19.
+//  Copyright © 2019 Danh Phu Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var moodTextField: UITextField!
+    
+    @IBAction func submitMoodbtn(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OpenWeatherMapAPI/WeatherViewController.swift
@@ -0,0 +1,33 @@
+//
+//  WeatherViewController.swift
+//  OpenWeatherMapAPI
+//
+//  Created by Ryan Nguyen on 8/7/19.
+//  Copyright © 2019 Danh Phu Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var moodLabel: UILabel!
+    @IBOutlet weak var weatherInfo: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
